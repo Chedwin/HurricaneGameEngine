72807ef2dc10d6702cd372f4fe970f6725ce23b2
Edwin Chen <edwin14chen@gmail.com>
2016-12-08 13:27:14 -0500

    Commit for Andrew

--- Hurricane/Hurricane/TestGame/SimulationScene.cpp
+++ Hurricane/Hurricane/TestGame/SimulationScene.cpp
@@ -17,6 +17,9 @@ SimulationScene::SimulationScene()
 	MODEL_MANAGER->LoadOBJ("HockeyStick", "models/hockeystick.obj");
 
 	MODEL_MANAGER->LoadOBJ("Puck", "models/Puck.obj");
+	MODEL_MANAGER->LoadAssimpModel("Planet", "models/planet.obj");
+	MODEL_MANAGER->LoadOBJ("Planet2", "models/planet.obj");
+	MODEL_MANAGER->LoadOBJ("Player", "models/player.obj");
 
 	TEXTURE_MANAGER->UploadTextureFromFile("textures/Puck.png", "PuckTexture");
 }
@@ -40,17 +43,24 @@ void SimulationScene::InitScene()
 	//planet->transform.position = ORIGIN;
 	//planet->Scale(VEC3(0.5f, 0.5f, 0.5f));
 
-	GameObject* hockeyStick = new GameObject(this, "Puck");
-	MeshComponent* hsMesh = new MeshComponent(hockeyStick, stdShader, "HockeyStick");
-	hockeyStick->Scale(VEC3(0.3f, 0.3f, 0.3f));
+	//GameObject* hockeyStick = new GameObject(this, "Puck");
+	//MeshComponent* hsMesh = new MeshComponent(hockeyStick, stdShader, "HockeyStick");
+	//hockeyStick->Scale(VEC3(0.3f, 0.3f, 0.3f));
 
 
-	GameObject* puck = new GameObject(this, "Sun");
-	MeshComponent* puckMesh = new MeshComponent(puck, stdShader, "Puck");
-	puck->Scale(VEC3(0.3f, 0.3f, 0.3f));
-	puck->Translate(VEC3(2.0f, 2.0f, 2.0f));
+	//GameObject* puck = new GameObject(this, "Sun");
+	//MeshComponent* puckMesh = new MeshComponent(puck, stdShader, "Puck");
+	//puck->Scale(VEC3(0.3f, 0.3f, 0.3f));
+	//puck->Translate(VEC3(2.0f, 2.0f, 2.0f));
 
-	hockeyStick->AddChild(puck);
+	//hockeyStick->AddChild(puck);
+
+	GameObject* player = new GameObject(this, "Player");
+	MeshComponent* playerMesh = new MeshComponent(player, stdShader, "Player");
+
+	player->Scale(VEC3(0.02, 0.02, 0.02));
+	player->Translate(VEC3(0, -1, 0));
+	mainCamera->AddChild(player);
 }
 
 void SimulationScene::Update(const hFLOAT _timeStep)
